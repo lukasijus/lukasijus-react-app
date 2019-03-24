6a14c4e66f8c31eb8cd3d721447fb33e25d4fed0
Lukas Rimkus <lukas.rimkus@procemex.com>
2019-03-24 11:23:49 +0200

    first commit

---
+++ precache-manifest.d830daf67c9bc883a4b1181e481d66f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "2eb06723998227a037a0",
+    "url": "/static/js/main.1d70f9fe.chunk.js"
+  },
+  {
+    "revision": "ccdf85a272b151ebd001",
+    "url": "/static/js/2.7b7f7f25.chunk.js"
+  },
+  {
+    "revision": "2eb06723998227a037a0",
+    "url": "/static/css/main.42c6d107.chunk.css"
+  },
+  {
+    "revision": "6433ef85b052bd6161bec53698955776",
+    "url": "/index.html"
+  }
+];
