ae28299d6cc4fceb6b2be62c61f8a68f0fb576b9
Lukas Rimkus <lukas.rimkus@procemex.com>
2019-03-24 12:43:48 +0200

    Updates

---
+++ precache-manifest.a2a8048fca165d9b2b735d9e32660532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lukasijus-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f5f6533fad009ec8feb9",
+    "url": "/lukasijus-react-app/static/js/runtime~main.98f95adc.js"
+  },
+  {
+    "revision": "4cb4f6107948c387f3ad",
+    "url": "/lukasijus-react-app/static/js/main.4e147515.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/lukasijus-react-app/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "4cb4f6107948c387f3ad",
+    "url": "/lukasijus-react-app/static/css/main.a33d884e.chunk.css"
+  },
+  {
+    "revision": "0956ab0e2c3792f98b60a802a4ffff58",
+    "url": "/lukasijus-react-app/index.html"
+  }
+];
