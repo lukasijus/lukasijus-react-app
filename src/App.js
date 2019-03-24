704949fb18239b7a2bc0379956f62c76f4aa878a
Lukas Rimkus <lukas.rimkus@procemex.com>
2019-03-24 13:01:53 +0200

    added link to website

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Lukasijus react app on gh-pages.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://lukasijus.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Checkout Lukasijus
           </a>
         </header>
       </div>
